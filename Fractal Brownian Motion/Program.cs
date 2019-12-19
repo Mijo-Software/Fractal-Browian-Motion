0d13be98e7674f97d2c57897bc4155a4a5fb8f52
Michael Johne <mjohne@users.noreply.github.com>
2019-12-19 15:17:12 +0100

    Add files via upload

--- Fractal Brownian Motion/Program.cs
+++ Fractal Brownian Motion/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Fractal_Brownian_Motion
 {
-  static class Program
-  {
-    /// <summary>
-    /// Der Haupteinstiegspunkt für die Anwendung.
-    /// </summary>
-    [STAThread]
-    static void Main()
-    {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainApp());
-    }
-  }
+	internal static class Program
+	{
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+			using (MainForm mainForm = new MainForm())
+			{
+				Application.Run(mainForm: mainForm);
+			}
+		}
+	}
 }
